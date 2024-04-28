9dea239a2fc3eb7d232f09645d1db9dfd997b3d2
NataliLebedieva <4155672@gmail.com>
2024-04-29 01:50:15 +0300

    com2

--- m1_hw1.py
+++ m1_hw1.py
@@ -1,7 +1,13 @@
-import datetime
+from datetime import datetime
 
-input_date = input("Введіть дату в форматі РРРР-ММ-ДД: ") 
+
 
 def get_days_from_today(date):
+    input_date_str = input("Введіть дату в форматі РРРР-ММ-ДД: ") 
+    input_date = datetime.strptime(input_date_str, '%Y.%m.%d')
+    date_today = datetime.today()
+    print(input_date_str, type(input_date_str))
+    print(input_date, type(input_date))
+    print((date_today).date())
+get_days_from_today()
     
-    return input_date
